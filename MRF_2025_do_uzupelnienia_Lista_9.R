c3fea637190da5edf68cc45cb363dd536a10a5c6
Paweł Skrzypczyński <275924@student.pwr.edu.pl>
2025-05-28 13:57:08 +0200

    lista 9 skończona

--- MRF_2025_do_uzupelnienia_Lista_9.R
+++ MRF_2025_do_uzupelnienia_Lista_9.R
@@ -25,44 +25,44 @@
 #   Napisz funkcję KalkulatorRatalny(TypeFlag = c("A", "B", "C", "D"), K, n, r)}
 #   zwracającą wektor wszystkich rat spłaty kredytu dla każdej z osób A,B,C,D.
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-KalkulatorRatalnt(TypeFlag= c("A", "B", "C", "D"), K, n, r){
-  if(TypeFlag="A"){
-    rata <- K/(1-(1+r)^(n+1)/(r))
+KalkulatorRatalny <- function(TypeFlag= c("A", "B", "C", "D"), K, n, r){
+  if(TypeFlag=="A"){
+    rata <- K*r*(1+r)^n/((1+r)^(n)-1)
     return(rep(rata, n))
   }
   
-  if(TypeFlag="B"){
-    rata <- rep(0,n)
+  rata <- rep(0,n)
+  
+  if(TypeFlag=="B"){
     stała <- K/n
-    for(i in 0:(n-1)){
-      rata[i] <- stała + r*(K-stała*i)
-      }
+    for(i in 1:(n)){
+      rata[i] <- stała + r*(K-stała*(i-1))
+    }
+    return(rata)
   }
   
-  if(TypeFlag="C"){
-    
+  if(TypeFlag=="C"){
+    for(i in 1:n-1){
+      rata[i] <- K*r
+    }
+    rata[n] <- K*(1+r)
+    return(rata)
+  }
+  
+  if(TypeFlag=="D"){
+    rata[n] <- K*(1+r)^n
+    return(rata)
   }
 }  
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+K <- 1000
+n <- 4
+r <- 0.1
+KalkulatorRatalny("A", K, n, r)
+KalkulatorRatalny("B", K, n, r) #najmniej do zapłaty
+KalkulatorRatalny("C", K, n, r)
+KalkulatorRatalny("D", K, n, r)
+#oddalanie terminy płatności powiększa koszty!
 #_______________________________________________________________________________
 #
 #------------------------------Zadanie 9.2--------------------------------------
@@ -81,30 +81,11 @@ KalkulatorRatalnt(TypeFlag= c("A", "B", "C", "D"), K, n, r){
 # Ile wyniesie RRSO w obydwu propozycjach i która z propozycji spłaty 
 # jest dla nas korzystniejsza?
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+library(FinCal)
+RRSO_1 <- irr(c(10000, 0, -6000, 0, -6000))
+RRSO_2 <- irr(c(10000, -3000, -3000, -3000, -3000))
+RRSO_1 #korzystniej.
+RRSO_2
 #_______________________________________________________________________________
 #
 #------------------------------Zadanie 9.3--------------------------------------
@@ -127,41 +108,13 @@ KalkulatorRatalnt(TypeFlag= c("A", "B", "C", "D"), K, n, r){
 #
 # Ile wynosi RRSO tego kredytu?
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ciąg_płatności <- numeric(6)
+ciąg_płatności[1] <- (200 - 6 - 2 - 1 - 0.02*200) * 10^3
+ciąg_płatności[2] <- (-40 - 0.04*200 - 0.02*160) * 10^3
+ciąg_płatności[3] <- (-40 - 0.04*160 - 0.02*120) * 10^3
+ciąg_płatności[4] <- (-40 - 0.04*120 - 0.02*80) * 10^3
+ciąg_płatności[5] <- (-40 - 0.04*80 - 0.02*40) * 10^3
+ciąg_płatności[6] <- (-40 - 0.04*40 - 0.02*0) * 10^3
+ciąg_płatności
+RRSO <- 100 * irr(ciąg_płatności)
+RRSO #%
